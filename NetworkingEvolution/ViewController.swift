e88cb16533d88244ef4400ef81db0556e1ddc21a
Austin Feight <austin@chexology.com>
2016-06-12 16:10:36 -0400

    Initial Commit

---
+++ NetworkingEvolution/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetworkingEvolution
+//
+//  Created by Austin Feight on 6/12/16.
+//  Copyright © 2016 Lost in Flight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
